5aa817426172a40d4cd16d347288791f46db18c2
kyoukouki <kyoukouki2003@gmail.com>
2025-09-08 14:13:44 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,19 @@
+import Head from 'next/head'
 import '@/styles/globals.css'
-export default function MyApp({ Component, pageProps }) { return <Component {...pageProps} /> }
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=ZCOOL+KuaiLe&family=Noto+Sans+SC:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="theme-color" content="#ffb3cf" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
