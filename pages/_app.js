fa27fc590bd3ebc0a92d24a3fe1d351cacfdb4f1
kyoukouki <kyoukouki2003@gmail.com>
2025-09-08 21:55:39 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,3 @@
-import Head from 'next/head'
-import '@/styles/theme.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=ZCOOL+KuaiLe&family=Noto+Sans+SC:wght@400;600&display=swap"
-          rel="stylesheet"
-        />
-        <meta name="theme-color" content="#ffb3cf" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
-}
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { supabase } from '@/lib/supabaseClient'
